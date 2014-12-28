2f84ddbbe3b204505e353ecc28f2ed3966ea5568
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2014-12-28 21:56:09 +0000

    ...

--- R/Analysis.R
+++ R/Analysis.R
@@ -1,23 +1,13 @@
 rm(list=ls(all=TRUE))
 
-library(xlsx)
-library(plyr)
-library(XLConnect)
-library(ggplot2)
-library(reshape)
-library(lme4)
-library(packrat)
-library(car)
-library(mvabund)
-library(proto)
-
+source("R/Packages.R")
 source("R/functions.R")
 ################
 # Process Data #
 ################
 source("R/FACE.vegetation.2014.management.R")
 
-# Row data for multi variate analysis
+# Raw data for multi variate analysis
 load("output/Data//FACE_Vegetation_Raw.RData")
 
 # Data frame with plant functional groups
