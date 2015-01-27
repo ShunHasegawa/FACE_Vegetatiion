b1b8d5b1945525dba5a4b9beb562c49cc03c0403
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-01-27 16:29:18 +0000

    unnessesary (probably) part is removed

--- R/Analysis.R
+++ R/Analysis.R
@@ -2,30 +2,17 @@ rm(list=ls(all=TRUE))
 
 source("R/Packages.R")
 source("R/functions.R")
+
 ################
 # Process Data #
 ################
-source("R/FACE.vegetation.2014.management.R")
+# source("R/FACE.vegetation.2014.management.R")
 
 # Raw data for multi variate analysis
 load("output/Data//FACE_Vegetation_Raw.RData")
 
 # Data frame with plant functional groups
 load("output//Data//FACE_Vegetation_PFG.RData")
-
-
-x$spp2 <- as.character(x$spp)
-x$spp2[grep("Paspalum", x$spp2)] <- "Paspalum spp"
-
-head(FACE.veg.rslt)
-str(FACE.veg.rslt$variable)
-FACE.veg.rslt$spp <- FACE.veg.rslt$variable
-
-as.character(FACE.veg.rslt$spp[grep("A", FACE.veg.rslt$spp)]) <- "A"
-unique(FACE.veg.rslt$spp)
-
-# co2 factor
-FACE.veg.rslt$co2 <- factor(ifelse(FACE.veg.rslt$ring %in% c(1, 4, 5), "elev", "amb"))
 
 ########
 # Figs #
