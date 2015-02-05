58f083cf545c70d8fbc254d323594552d77aadef
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-02-05 15:39:13 +0000

    figure for biodiversity indices

--- R/Figs.R
+++ R/Figs.R
@@ -68,3 +68,100 @@ p <- Orgnplt +
   facet_grid(co2 ~ form, scale = "free_x", space = "free_x", labeller = label_parsed, margins= "form") 
 ggsavePP(filename = "output/figs/FACE_Origin_CO2", plot = p, width= 8, height = 6)
 
+#####################
+# Figure for thesis #
+#####################
+# C4:C3 & legume:Non_legume
+
+PfgPlotSum <- ddply(veg, .(year, ring, plot, PFG), summarise, value = sum(value))
+
+# subset required rows
+CL_PfgPlotSum <- subsetD(PfgPlotSum, PFG %in% c("c3", "c4", "legume", "Non_legume"))
+
+pfgR <- ddply(CL_PfgPlotSum, .(year, ring, plot),
+              function(x) {
+               c43R <- with(x, value[PFG == "c4"]/value[PFG == "c3"])
+               lgNonlgR <- with(x, value[PFG == "legume"]/value[PFG == "Non_legume"])
+               # there one c4 observation which is 0, so put c4 on numerator
+               return(data.frame(c43R, lgNonlgR))
+             })
+
+# native:introduced
+OgnPlotSum <- ddply(veg, .(year, ring, plot, origin), summarise, value = sum(value))
+orgn <- ddply(subset(OgnPlotSum, origin %in% c("naturalised", "native")), 
+                     .(year, ring, plot),
+                     function(x){
+                       IntNatR <- with(x, value[origin == "naturalised"]/value[origin == "native"])
+                       return(data.frame(IntNatR))
+                     })
+
+# biodiversity indices
+summary(veg.face)
+
+SiteName <- c("year", "ring", "plot", "position", "cell")
+SppName <- names(veg.face)[!names(veg.face) %in% SiteName]
+
+plt.veg <- ddply(veg.face, .(year, ring, plot), function(x) colSums(x[, SppName]))
+
+vegDF <- plt.veg[, SppName]
+siteDF <- plt.veg[, c("year", "ring", "plot")]
+
+DivDF <- within(siteDF,{
+  H <- diversity(vegDF) # Shannon's index
+  S <- specnumber(vegDF) # number of spp
+  J <- H/log(S)  # Pielou's evenness
+})
+
+# merge above data frames
+BioDivDF <- Reduce(function(...) merge(..., by = c("year", "ring", "plot")), 
+                   list(pfgR, orgn, DivDF))
+
+# melt for making figure
+BioDivDF_mlt <- melt(BioDivDF, id = c("year", "ring", "plot"))
+
+# Mean and Se
+RngSmmryBioDivDF <- ddply(BioDivDF_mlt, .(year, ring, variable), summarise, 
+                          value = mean(value, na.rm = TRUE))
+RngSmmryBioDivDF <- within(RngSmmryBioDivDF, {
+  co2 = factor(ifelse(ring %in% c(1, 4, 5), "elev", "amb"))
+  block = recode(ring, "1:2 = 'A'; 3:4 = 'B'; 5:6 = 'C'")
+})
+
+SmmryBioDivDF <- ddply(RngSmmryBioDivDF, .(year, co2, variable), summarise,
+                       Mean = mean(value),
+                       SE = ci(value)[4],
+                       N = sum(!is.na(value)))
+
+# make a plot
+
+# define graphic background
+science_theme <- theme(panel.grid.major = element_blank(), 
+                       panel.grid.minor = element_blank(), 
+                       legend.position = c(.91, .91),
+                       # legend.text = element_text(size = 2),
+                       legend.title = element_blank())
+
+# change variable names
+vars <- c("C4:C3", "Legume:Non_legume",
+          "Introduced:Native", "Evenness", "Species Richness", "Diversity (H')")
+df <- within(SmmryBioDivDF, {
+  variable <- factor(variable, 
+                     levels = c("c43R", "lgNonlgR", "IntNatR", "J", "S", "H"),
+                     labels = vars)
+  year <- factor(year, levels = c("2012", "2014"), 
+                 labels = c("2013\n(Pre-CO2)", "2014"))
+})
+
+p <- ggplot(df, aes(x = year, y = Mean, group = co2))
+p2 <- p + 
+  geom_errorbar(aes(x = year, ymin = Mean - SE, ymax = Mean + SE), 
+                        position = position_dodge(.5),
+                width = 0) +
+  geom_point(aes(fill = co2, shape = co2), position = position_dodge(.5), size = 5) + 
+  labs(x = "Year", y = NULL) + 
+  scale_shape_manual(values = c(21, 21), labels = c("Ambient", expression(eCO[2]))) +
+  scale_fill_manual(values = c("black", "white"), 
+                    labels = c("Ambient", expression(eCO[2]))) +
+  facet_wrap(~variable, scales = "free_y") +
+  science_theme
+ggsavePP(filename = "output/figs/FACE_CO2_Biodiversity", width = 6, height = 5, plot = p2)
