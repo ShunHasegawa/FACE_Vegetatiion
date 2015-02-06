e61b99029b2f031d0c389ab116b26f124c983102
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-02-06 01:16:25 +0000

    dissimilarity and PCO figs are added

--- R/Figs.R
+++ R/Figs.R
@@ -71,6 +71,11 @@ ggsavePP(filename = "output/figs/FACE_Origin_CO2", plot = p, width= 8, height = 
 #####################
 # Figure for thesis #
 #####################
+
+##########################
+## biodiversity indices ##
+##########################
+
 # C4:C3 & legume:Non_legume
 
 PfgPlotSum <- ddply(veg, .(year, ring, plot, PFG), summarise, value = sum(value))
@@ -165,3 +170,189 @@ p2 <- p +
   facet_wrap(~variable, scales = "free_y") +
   science_theme
 ggsavePP(filename = "output/figs/FACE_CO2_Biodiversity", width = 6, height = 5, plot = p2)
+
+#################################
+## Dissimilarity between years ##
+#################################
+
+# all spp ----
+
+# Compute dissimiliraity for each plot between 2012 and 2013
+disDF_spp <- ddply(plt.veg, .(ring, plot), 
+               function(x) vegdist(x[, SppName], method = "altGower"))
+names(disDF_spp)[names(disDF_spp) == "V1"] <- "Dissim"
+disDF_spp$variable <- "AllSpp"
+
+# PFG ----
+summary(PfgPlotSum)
+
+# remove NA and Lichen(which is not assessed in the 2nd year)
+PFGdf <- subsetD(PfgPlotSum, !PFG %in% c(NA, "Lichen"))
+
+# cast to create matrix-like data frame showing community compoisition
+PFGdf_cst <- dcast(year + ring + plot ~ PFG, data = PFGdf)
+
+# Compute dissimiliraity for each plot between 2012 and 2013
+pfgName <- names(PFGdf_cst)[!names(PFGdf_cst) %in% c("year", "ring", "plot")]
+
+disDF_pfg <- ddply(PFGdf_cst, .(ring, plot), 
+                   function(x) 
+                     vegdist(x[, pfgName], method = "altGower"))
+names(disDF_pfg)[names(disDF_pfg) == "V1"] <- "Dissim"
+disDF_pfg$variable <- "PFGs"
+
+# make plots ----
+# merge the above data frames
+disDF <- rbind(disDF_spp, disDF_pfg)
+
+# organise data frame
+disDF <- within(disDF, {
+  co2 = factor(ifelse(ring %in% c(1, 4, 5), "elev", "amb"))
+})
+
+# ring mean
+RngDisDF <- ddply(disDF, .(co2, ring, variable), summarise, Dissim = mean(Dissim))
+
+# co2 mean, SE
+SmmryDisDF <- ddply(RngDisDF, .(co2, variable), summarise,
+                    Mean = mean(Dissim),
+                    SE = ci(Dissim)[4],
+                    N = sum(!is.na(Dissim)))
+# plot
+
+# change variable names
+df <- within(SmmryDisDF, {
+  co2 = factor(co2, levels = c("amb", "elev"), 
+               labels = c("Ambient", expression(eCO[2])))
+})
+p <- ggplot(data = SmmryDisDF, aes(x = co2, y = Mean))
+p2 <- p +
+  geom_errorbar(aes(x = co2, ymin = Mean - SE, ymax = Mean + SE)) +
+  geom_point(aes(shape = co2, fill = co2),size = 5) +
+  scale_x_discrete(labels = c("Ambient", expression(eCO[2]))) +
+  scale_shape_manual(values = c(21, 21)) +
+  scale_fill_manual(values = c("black", "white")) +
+  labs(x = NULL, y = "Dissimilarity between 2013 and 2014") +
+  facet_wrap(~ variable, scale = "free_y") +
+  science_theme +
+  theme(legend.position = "none")
+ggsavePP(filename = "output/figs/FACE_Vegetation_Dissimilarity", 
+         width = 4, height = 3, plot = p2)
+
+# Dissimilarity against moisture ----
+
+# load soil moisture data
+load("Data/FACE_TDR_ProbeDF.RData")
+summary(FACE_TDR_ProbeDF)
+soilDF <- subsetD(FACE_TDR_ProbeDF, Sample == "vegetation" & 
+                    Date <= as.Date("2013-12-31") &
+                    Date >= as.Date("2013-1-1"))
+soilDF$plot <- as.factor(soilDF$plot)
+MoistDF <- ddply(soilDF, .(ring, plot), summarise, Moist = mean(Moist, na.rm = TRUE))
+plot(Moist ~ ring, data = MoistDF)
+
+# merge data frame
+Dis_MoistDF <- merge(disDF, MoistDF, by = c("ring", "plot"))
+
+# regression line for all spp and produce predicted variables
+m1 <- lm(log(Dissim) ~ log(Moist), data = subsetD(Dis_MoistDF, variable == "AllSpp"))
+xv <- seq(min(Dis_MoistDF$Moist), max(Dis_MoistDF$Moist), length.out = 100)
+yv <- exp(predict(m1, list(Moist = xv)))
+PreDF <- data.frame(Moist = xv, Mean = yv, variable = "AllSpp", co2 = "amb")
+
+# plot
+p <- ggplot(data = Dis_MoistDF, aes(x = Moist, y = Dissim, shape = co2, fill = co2))
+p2 <- p + 
+  geom_point(size = 2) + 
+  facet_grid(variable ~ ., scales = "free_y") +
+  scale_shape_manual(values = c(24, 21), labels = c("Ambient", expression(eCO[2]))) +
+  scale_fill_manual(values = c("black", "white"), labels = c("Ambient", expression(eCO[2]))) +
+  geom_line(aes(x = Moist, y = Mean), data = PreDF) +
+  labs(x = "Soil moisture contents (%)",
+       y = "Dissimilarity between 2013 and 2014") +
+  science_theme +
+  theme(legend.position = c(.8, .89))
+ggsavePP(filename = "output/figs/FACE_DissmVs.Moist", width = 4, height = 4, 
+         plot = p2)
+
+#######
+# PCO #
+#######
+
+# ring sum
+SppName
+RngVegdf <- ddply(veg.face, .(year, ring), function(x) colSums(x[, SppName]))
+RngSppDF <- RngVegdf[, SppName]
+RngSiteDF <- RngVegdf[, !names(RngVegdf) %in% SppName]
+RngSiteDF$co2 <- factor(ifelse(RngSiteDF$ring %in% c(1, 4, 5), "elev", "amb"))
+
+# peform PCO
+
+# pco <- cmdscale(d = vegdist(RngSppDF, method = "altGower"), eig = TRUE)
+
+pco <- cmdscale(d = vegdist(log(RngSppDF + 1), method = "bray"), eig = TRUE, k = 3)
+pco$GOF
+pco$eig
+ExpVars <- round(pco$eig[1:3] * pco$GOF[2]/(sum(pco$eig[1:3])) * 100, 2)
+
+# Note: GOF is given as belos 
+# pco$GOF[1] at k = 3
+sum(pco$eig[1:3])/sum(abs(pco$eig))
+
+# pco$GOF[2] at k = 2
+eig2 <- ifelse(pco$eig < 0, 0, pco$eig)
+sum(pco$eig[1:3])/sum(eig2)
+
+# pco <- cmdscale(d = vegdist(log(RngSppDF + 1), method = "bray"), eig = TRUE, k = 11)
+
+
+# this transformation and dissimilarity is more interpretable than above...
+
+pcos <- cbind(RngSiteDF, pco1 = pco$points[, 1], 
+                          pco2 = pco$points[, 2],
+                          pco3 = pco$points[, 3])
+pcoDF <- melt(pcos, id = c("year", "ring", "co2", "pco1"))
+pcoDF$variable <- factor(pcoDF$variable, levels = c("pco2", "pco3"), 
+                         labels = paste("PCO",  c(2, 3), " (", ExpVars[2:3], " %)", sep = ""))
+
+# make plots ----
+
+# connect the same ring in different years
+p <- ggplot(data = pcoDF, aes(x = pco1, y = value, shape = year, col = co2))
+p2 <- p + 
+  geom_point(size = 3) + 
+  scale_color_manual(values = c("blue", "red"), labels = c("Ambient", expression(eCO[2]))) +
+  science_theme +
+  labs(x = paste("PCO1 (", ExpVars[1]," %)", sep = ""), 
+       y = "PCO axis") +
+  theme(legend.position = "top",
+        legend.box = "horizontal",
+        legend.box.just = "left") +
+  facet_grid(. ~variable)
+p3 <- p2 + geom_line(aes(group = ring))
+ggsavePP(filename = "output/figs/FACE_Vegetation_PCO_Ring", width = 4, height = 4, 
+         plot = p3)
+
+# make area for each pairs of amb and elev for each year
+chulDF <- ddply(pcoDF, .(year, co2, variable), 
+                function(x) {chx <- chull(x[c("pco1", "value")]) 
+                             chxDF <- data.frame(rbind(x[chx,], x[chx[1], ]))
+                             return(chxDF)})
+p4 <- p2 + geom_polygon(data = chulDF, alpha = .1)
+ggsavePP(filename = "output/figs/FACE_Vegetation_PCO_CO2", width = 4, height = 4, 
+         plot = p4)
+
+# different colors for each ring
+p <- ggplot(data = pcoDF, aes(x = pco1, y = value, shape = year, col = ring))
+p2 <- p + 
+  geom_point(size = 3) + 
+  geom_line(aes(group = ring)) +
+  science_theme +
+  labs(x = paste("PCO1 (", ExpVars[1]," %)", sep = ""), 
+       y = "PCO axis") +
+  theme(legend.position = "top",
+        legend.box = "horizontal",
+        legend.box.just = "left") +
+  facet_grid(. ~variable) 
+ggsavePP(filename = "output/figs/FACE_Vegetation_PCO_EachRing", width = 4, height = 4, 
+         plot = p2)
