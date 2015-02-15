5fcddf584cd3a34c64e48e05ecf0e01793f2990d
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-02-15 16:31:20 +0000

    rda looks slightly better maybe I should try different transformation

--- R/CAP.analysis.R
+++ R/CAP.analysis.R
@@ -13,68 +13,120 @@
 DisMatrix_Year <- dlply(RingSumVeg, .(year), 
                         function(x) vegdist(x[, SppName], method = "altGower")) # ln(x + 1)
 
-# 2012 (due to the environmental setting in CAPdiscrim,, can't use llply)
-Dis2012 <- DisMatrix_Year[[1]]
-Cap2012 <-  CAPdiscrim(Dis2012 ~ co2, 
-                       data = subset(RingSumVeg, year == 2012), 
-                       permutations = 1000)
-# 2014
-Dis2014 <- DisMatrix_Year[[1]]
-Cap2014 <-  CAPdiscrim(Dis2014 ~ co2, 
-                       data = subset(RingSumVeg, year == 2014), 
-                       permutations = 1000)
 
-# merge
-CapYeardf <- data.frame(CAP1 = c(Cap2012$x, Cap2014$x),
-                        rbind(subset(RingSumVeg, year == 2012), 
-                              subset(RingSumVeg, year == 2014))[, c("year", "co2")])
+# Run cap for each dataset.
+# Note that llply(or lapply) can't be used as a matrix object has to be difined
+# out side of the function where CAPdiscrim is used (it's really weird
+# though..... it should be got to do with enrironment setting. but don't know
+# how to difine environment at the moment. so just use this for time being)
+
+capList <- list()
+
+for (i in 1:2){
+  disMatrix <- DisMatrix_Year[[i]]
+  capList[[i]] <- CAPdiscrim(disMatrix ~ co2, 
+                                 data = subset(RingSumVeg, 
+                                               year == names(DisMatrix_Year)[i]),
+                                 permutations = 1000)
+}
+
 # create a plot
+names(capList) <- c("2012", "2014")
+
+CapYeardf <- ldply(names(capList), function(x) {
+  lst <- capList[[x]]
+  data.frame(CAP1 = lst$x[, 1], co2 = lst$group, year = x)
+})
+  
 p <- ggplot(CapYeardf, aes(x = co2, y = CAP1))
 p + geom_point(size = 4) + facet_grid(.~year)
 
+# each co2 treatment separately----
 
+# create distance matrix
+DisMatrix_co2 <- dlply(RingSumVeg, .(co2), 
+                       function(x) vegdist(x[, SppName], method = "altGower")) # ln(x + 1)
 
-disMatrix <- DisMatrix_Year[["2012"]]
-capList <- llply(names(DisMatrix_Year),
-                 function(x) {
-                   disMatrix <- DisMatrix_Year[[x]]
-                   environment(disMatrix) <- .GlobalEnv
-                   CAPdiscrim(disMatrix ~ co2, 
-                              data = subset(RingSumVeg, year == x),permutations = 1000)
+# run CAP
+capList_co2 <- list()
+
+for (i in 1:2){
+  disMatrix <- DisMatrix_co2[[i]]
+  capList_co2[[i]] <- CAPdiscrim(disMatrix ~ year, 
+                        data = subset(RingSumVeg, co2 == names(DisMatrix_co2)[i]),
+                        permutations = 1000)
+}
+
+# create a plot
+names(capList_co2) <- c("amb", "elev")
+
+CapCo2df <- ldply(names(capList_co2), function(x) {
+  lst <- capList_co2[[x]]
+  data.frame(CAP1 = lst$x[, 1], year = lst$group, co2 = x)
 })
 
-# run cap for each dataset. Note that a matrix (or data frame) has to be 
-# deifined outside of the function where CAPdiscrim is used; hence disMatrix is 
-# difined out side of f() below. (it's really weird though..... it should be got
-# to do with enrironment setting. but don't know how to difine environment at
-# the moment. so just use this for time being)
-capList <- llply(names(DisMatrix_Year),
-                 function(x) {
-                   disMatrix <- DisMatrix_Year[[x]]
-                   f <- function(){
-                     CAPdiscrim(disMatrix ~ co2, 
-                                data = subset(RingSumVeg, year == x),
-                                permutations = 1000)
-                   }
-                   return(f())})
+p <- ggplot(CapCo2df, aes(x = year, y = CAP1))
+p + geom_point(size = 4) + facet_grid(.~co2)
 
+#######
+# PFG #
+#######
+
+# each year separately----
+
+# create distance matrix
+PFG_DisMatrix_Year <- dlply(RingSumPFGMatrix, .(year), 
+                            function(x) vegdist(x[, PFGName], method = "altGower")) # ln(x + 1)
+
+PFGcapList <- list()
+
+for (i in 1:2){
+  disMatrix <- PFG_DisMatrix_Year[[i]]
+  PFGcapList[[i]] <- CAPdiscrim(disMatrix ~ co2, 
+                             data = subset(RingSumPFGMatrix,
+                                           year == names(PFG_DisMatrix_Year)[i]),
+                             permutations = 1000)
+}
+
+# create a plot
+names(PFGcapList) <- c("2012", "2014")
+
+PFGCapYeardf <- ldply(names(PFGcapList), function(x) {
+  lst <- PFGcapList[[x]]
+  data.frame(CAP1 = lst$x[, 1], co2 = lst$group, year = x)
+})
+
+p <- ggplot(PFGCapYeardf, aes(x = co2, y = CAP1))
+p + geom_point(size = 4) + facet_grid(.~year)
+
+# each co2 treatments separately----
+
+# create distance matrix
+PFG_DisMatrix_co2 <- dlply(RingSumPFGMatrix, .(co2),
+                           function(x) vegdist(x[, PFGName], method = "bray")) # ln(x + 1)
+
+PFGcapList_co2 <- list()
+
+for (i in 1:2){
+  disMatrix <- PFG_DisMatrix_co2[[i]]
+  PFGcapList_co2[[i]] <- CAPdiscrim(disMatrix ~ year, 
+                                    data = subset(RingSumPFGMatrix,
+                                                  co2 == names(PFG_DisMatrix_co2)[i]),
+                                permutations = 1000)
+}
 
 
 # create a plot
-ldDF <- data.frame(sites, ld1 = capDF$x[, 1], ld2 = capDF$x[, 2])
-chulDF <- ddply(ldDF, .(year, ring), 
-                function(x) {chx <- chull(x[c("ld1", "ld2")]) 
-                             chxDF <- data.frame(rbind(x[chx,], x[chx[1], ]))
-                             return(chxDF)})
+names(PFGcapList_co2) <- c("amb", "elev")
 
+PFGCapCo2df <- ldply(names(PFGcapList_co2), function(x) {
+  lst <- PFGcapList_co2[[x]]
+  data.frame(CAP1 = lst$x[, 1], year = lst$group, co2 = x)
+})
 
-
-theme_set(theme_bw())
-p <- ggplot(ldDF, aes(x = ld1, y = ld2, col = ring, shape = year))
-p + geom_point(size = 5) + geom_polygon(data = chulDF, alpha = .1)
-
-# eco2 vegeataion seems to shift towards left along ld1 in 2014
-boxplot(ld1 ~ year*ring, data = ldDF)
+p <- ggplot(PFGCapCo2df, aes(x = year, y = CAP1))
+p + geom_point(size = 4) + 
+  facet_grid(.~co2)
 
 # vegan
 vegDF <- capscale(transDF ~ YR, sites, dist = "bray")
