6b7a6b51055b2e15964d02cbcfb8b38e8b9948df
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-03-25 11:07:18 +0000

    data set including 2015 is used

--- R/Analysis.R
+++ R/Analysis.R
@@ -6,19 +6,19 @@ source("R/functions.R")
 ################
 # Process Data #
 ################
-# source("R/FACE.vegetation.2014.management.R")
+source("R/FACE_VegetationDataSheet2015.R")
 
 # Raw data for multi variate analysis
-load("output/Data//FACE_Vegetation_Raw.RData")
+load("output/Data//FACE_Vegetation_Raw_2013_2015.RData")
 
 # Data frame with plant functional groups
-load("output//Data//FACE_Vegetation_PFG.RData")
+load("output//Data//FACE_Vegetation_PFG_2015.RData")
 
-# remove unknown spp
-veg <- FACE.veg.rslt[!grepl("Unknown", FACE.veg.rslt$variable), ]
+# check unknown spp
+all(!grepl("unknown", VegRes15$variable, ignore.case = TRUE))
 
 # co2, block and id
-veg <- within(veg, {
+veg <- within(VegRes15, {
   block <- recode(ring, "1:2 = 'A'; 3:4 = 'B'; 5:6 = 'C'")
   co2 <- factor(ifelse(ring %in% c(1, 4, 5), "elev", "amb"))
   id <- ring:plot
