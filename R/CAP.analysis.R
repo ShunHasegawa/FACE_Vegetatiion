bfdf053c9996d28c418f2d5ff1192f50589a016a
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-03-26 01:11:11 +0000

    canonical correlation values are added to figures

--- R/CAP.analysis.R
+++ R/CAP.analysis.R
@@ -23,10 +23,13 @@ capList <- list()
 
 for (i in 1:3){
   disMatrix <- DisMatrix_Year[[i]]
-  capList[[i]] <- CAPdiscrim(disMatrix ~ co2, 
-                                 data = subset(RingSumVeg, 
-                                               year == names(DisMatrix_Year)[i]),
-                                 permutations = 1000)
+  envDF <- subset(RingSumVeg, year == names(DisMatrix_Year)[i])
+  capList[[i]] <- CAPdiscrim(disMatrix ~ co2, data = envDF, permutations = 9999)
+  
+  # add canonical correlation
+  capList[[i]]$CanonicalCorSq <- CanonicalCor(CAPRes = capList[[i]], EnvDF = envDF, 
+                                              term = "co2") 
+  rm(disMatrix, envDF)
 }
 
 # create a plot
@@ -34,18 +37,27 @@ names(capList) <- c("2012", "2014", "2015")
 
 CapYeardf <- ldply(names(capList), function(x) {
   lst <- capList[[x]]
-  data.frame(CAP1 = lst$x[, 1], co2 = lst$group, year = x)
-})
-  
+  sqCor <- format(lst$CanonicalCorSq, digits = 3, nsmall = 3)
+  data.frame(CAP1 = lst$x[, 1], co2 = lst$group, year = paste(x, sqCor, sep = "_"))
+    # year: create labels for facet_grid showing year with associated canonical  
+    # correlations. See notes below in more detail.
+  })
+
 p <- ggplot(CapYeardf, aes(x = co2, y = CAP1))
-p + geom_point(size = 4) + facet_grid(.~year)
+p2 <- p + geom_point(size = 4) + 
+  facet_grid(.~year, 
+             labeller = label_bquote(.(strsplit(x, "_")[[1]][1])~(sigma^2==.(strsplit(x, "_")[[1]][2])))) 
+# I need to two values to create labels: year and canonical correlation, but
+# label_bquote can only take one variable. Hence I concatenated those two above
+# (e.g. 2012_0.597). Now I split this using strsplit and call each element
+# separately within label_bquote (e.g.strsplit(x, "_")[[1]][1] = "2012")
+ggsavePP(filename = "output//figs/FACE_CAPvsCO2_byYear", plot = p2,  width = 6, height = 4)
 
 # each co2 treatment separately----
 
 # create distance matrix
 DisMatrix_co2 <- dlply(RingSumVeg, .(co2), 
-                       function(x) vegdist(log(x[, SppName] + 1), 
-                                           method = "bray"))
+                       function(x) vegdist(log(x[, SppName] + 1), method = "bray"))
 
 # run CAP
 capList_co2 <- list()
@@ -56,7 +68,12 @@ for (i in 1:2){
   capList_co2[[i]] <- CAPdiscrim(disMatrix ~ year, 
                         data = envDF,
                         permutations = 1000)
+  # add canonical correlation
+  capList_co2[[i]]$CanonicalCorSq <- CanonicalCor(CAPRes = capList_co2[[i]], EnvDF = envDF, 
+                                                  term = "year") 
+  # spp correlation
   capSppScore_co2[[i]] <- add.spec.scores(capList_co2[[i]], log(envDF[, SppName] + 1))$cproj
+  rm(disMatrix, envDF)
 }
 
 # create a plot
@@ -66,24 +83,41 @@ names(capSppScore_co2) <- c("amb", "elev")
 # site scores
 CapCo2df <- ldply(c("amb", "elev"), function(x) {
   lst <- capList_co2[[x]]
-  data.frame(CAP1 = lst$x[, 1], CAP2 = lst$x[, 2], year = lst$group, co2 = x)
+  sqCor <- format(lst$CanonicalCorSq, digits = 1, nsmall = 3)
+  data.frame(CAP1 = lst$x[, 1], CAP2 = lst$x[, 2], year = lst$group, 
+             co2 = paste(x, sqCor[1], sqCor[2], sep = "_"))
 })
 
 # species score (correlation between ld1 and species abundance (after
 # transformation))
 CapCo2SppScore <- ldply(c("amb", "elev"), function(x) {
   df <- capSppScore_co2[[x]]
-  spdf <- data.frame(CAP1 = df[, 1], CAP2 = df[, 2], Spp = row.names(df), co2 = x, year = "Scaled species Score")
+  spdf <- data.frame(CAP1 = df[, 1], CAP2 = df[, 2], 
+                     Spp = row.names(df), 
+                     co2 = x, 
+                     year = "Scaled species Score")
   spdf <- spdf[complete.cases(spdf), ]
   # set threshhold for correlation
   subset(spdf, abs(CAP1) > .6|abs(CAP2) > .6)
 })  
+CapCo2SppScore$co2 <- factor(CapCo2SppScore$co2, labels = levels(CapCo2df$co2))
 
 p <- ggplot(CapCo2df, aes(x = CAP1, y = CAP2, fill = year, col = year))
-p + geom_point(size = 4) + 
+p2 <- p + geom_point(size = 2) + 
   geom_text(data = CapCo2SppScore, aes(x = CAP1 * 6, y = CAP2 * 2, label = Spp),
-            size = 4) +
-  facet_grid(.~co2)
+            size = 2) +
+  facet_grid(.~co2,
+             labeller = label_bquote(.(strsplit(x, "_")[[1]][1])~
+                                       (list(sigma[1]^2==.(strsplit(x, "_")[[1]][2]),
+                                             sigma[2]^2==.(strsplit(x, "_")[[1]][3])
+                                             ) # list is used to insert ","
+                                        )
+                                     )
+             ) +
+  theme(legend.position = "top", 
+        legend.direction = "horizontal")
+
+ggsavePP(filename = "output//figs/FACE_CAPvsYear_byCO2", plot = p2,  width = 6, height = 6)
 
 #######
 # PFG #
@@ -99,10 +133,14 @@ PFGcapList <- list()
 
 for (i in 1:3){
   disMatrix <- PFG_DisMatrix_Year[[i]]
+  envDF <- subset(RingSumPFGMatrix, year == names(PFG_DisMatrix_Year)[i])
   PFGcapList[[i]] <- CAPdiscrim(disMatrix ~ co2, 
-                             data = subset(RingSumPFGMatrix,
-                                           year == names(PFG_DisMatrix_Year)[i]),
+                             data = envDF,
                              permutations = 1000)
+  # add canonical correlation
+  PFGcapList[[i]]$CanonicalCorSq <- CanonicalCor(CAPRes = PFGcapList[[i]],
+                                                 EnvDF = envDF, term = "co2") 
+  rm(disMatrix, envDF)
 }
 
 # create a plot
@@ -110,11 +148,18 @@ names(PFGcapList) <- c("2012", "2014", "2015")
 
 PFGCapYeardf <- ldply(names(PFGcapList), function(x) {
   lst <- PFGcapList[[x]]
-  data.frame(CAP1 = lst$x[, 1], co2 = lst$group, year = x)
+  sqCor <- format(lst$CanonicalCorSq, digits = 1, nsmall = 3)
+  data.frame(CAP1 = lst$x[, 1], co2 = lst$group, year = paste(x, sqCor, sep = "_"))
 })
 
 p <- ggplot(PFGCapYeardf, aes(x = co2, y = CAP1))
-p + geom_point(size = 4) + facet_grid(.~year)
+p2 <- p + geom_point(size = 4) + 
+  facet_grid(.~year, 
+             labeller = label_bquote(.(strsplit(x, "_")[[1]][1])~
+                                       (sigma^2==.(strsplit(x, "_")[[1]][2])))) 
+
+ggsavePP(filename = "output//figs/FACE_CAPvsCO2_byYear_PFG", 
+         plot = p2,  width = 6, height = 4)
 
 # each co2 treatments separately----
 
@@ -130,6 +175,11 @@ for (i in 1:2){
   disMatrix <- PFG_DisMatrix_co2[[i]]
   envDF <- subset(RingSumPFGMatrix, co2 == names(PFG_DisMatrix_co2)[i])
   PFGcapList_co2[[i]] <- CAPdiscrim(disMatrix ~ year, data = envDF, permutations = 1000)
+  # add canonical correlation
+  PFGcapList_co2[[i]]$CanonicalCorSq <- CanonicalCor(CAPRes = PFGcapList_co2[[i]],
+                                                     EnvDF = envDF, 
+                                                     term = "year") 
+  # spp correlation
   PFGcapSppScore_co2[[i]] <- add.spec.scores(PFGcapList_co2[[i]], log(envDF[, PFGName] + 1))$cproj
   rm(envDF, disMatrix)
 }
@@ -140,7 +190,9 @@ names(PFGcapSppScore_co2) <- c("amb", "elev")
 
 PFGCapCo2df <- ldply(names(PFGcapList_co2), function(x) {
   lst <- PFGcapList_co2[[x]]
-  data.frame(CAP1 = lst$x[, 1], CAP2 = lst$x[, 2], year = lst$group, co2 = x)
+  sqCor <- format(lst$CanonicalCorSq, digits = 1, nsmall = 3)
+  data.frame(CAP1 = lst$x[, 1], CAP2 = lst$x[, 2], year = lst$group, 
+             co2 = paste(x, sqCor[1], sqCor[2], sep = "_"))
 })
 
 # species score
@@ -154,11 +206,25 @@ PFGCapCo2SppScore <- ldply(c("amb", "elev"), function(x) {
   spdf <- spdf[complete.cases(spdf), ]
   spdf
 })  
+PFGCapCo2SppScore$co2 <- factor(PFGCapCo2SppScore$co2, labels = levels(PFGCapCo2df$co2))
 
 p <- ggplot(PFGCapCo2df, aes(x = CAP1, y = CAP2, col = year))
-p + geom_point(size = 4) + 
-  geom_text(data = PFGCapCo2SppScore, aes(x = CAP1 * 2.5, y = CAP2, label = PFG)) +
-  facet_grid(.~co2)
+p2 <- p + geom_point(size = 4) + 
+  geom_text(data = PFGCapCo2SppScore, 
+            aes(x = CAP1 * 2.5, y = CAP2, label = PFG), 
+            size = 3) +
+  facet_grid(.~co2,
+             labeller = label_bquote(.(strsplit(x, "_")[[1]][1])~
+                                       (list(sigma[1]^2==.(strsplit(x, "_")[[1]][2]),
+                                             sigma[2]^2==.(strsplit(x, "_")[[1]][3])
+                                       ) # list is used to insert ","
+                                       )
+             )
+  ) +
+  theme(legend.position = "top", 
+        legend.direction = "horizontal")
+
+ggsavePP(filename = "output//figs/FACE_CAPvsYear_byCO2_PFG", plot = p2,  width = 6, height = 6)
 
 # save all objects to create summary document later
 save.image(file = "output//Data/CAP_Object.RData")
