471c2a0dd7aafdef5b3c3f493e5fef5ee16b7966
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-03-26 17:09:29 +0000

    c3, c4 proportion is analysed with 2015 result

--- R/Stats_C3_4_Grass.R
+++ R/Stats_C3_4_Grass.R
@@ -1,14 +1,14 @@
-head(FACE.veg.rslt)
+head(veg)
 
 # Create C3, C4 grass summary DF
 
 # PFG_plot sum for c3 and c4 grass wihtought unknown spp
-PFGPltSum <- ddply(subsetD(FACE.veg.rslt, form == "Grass" & PFG %in% c("c3", "c4")),
+PFGPltSum <- ddply(subsetD(veg, form == "Grass" & PFG %in% c("c3", "c4")),
                   .(year, co2, block, ring, plot, id, PFG), summarise, 
                   value = sum(value, na.rm = TRUE))
 
 # cast and make data frame for anlysis
-C34grassDF <- cast(PFGPltSum, year + co2 + block +  ring + plot + id ~ PFG)
+C34grassDF <- dcast(PFGPltSum, year + co2 + block + ring + plot + id ~ PFG)
 
 # glm
 C34grassDF$yv <- cbind(C34grassDF$c3, C34grassDF$c4)
@@ -21,9 +21,6 @@ summary(glm1)
   # highly overdispersed
 glm2 <- glm(yv ~ year * co2, data = C34grassDF, family = quasibinomial)
 summary(glm2)
-glm3 <- update(glm2, ~. - year:co2)
-anova(glm2, glm3, test = "Chisq")
-anova(glm2, glm3, test = "F")
 # no co2 effect
 
 ##########################
@@ -33,13 +30,14 @@ anova(glm2, glm3, test = "F")
 # ids)
 C34grassDF$ResID <- factor(1:nrow(C34grassDF))
 
-m1 <- glmer(yv ~ year * co2
-            +  (1|block) +(1|ring) + (1|id), 
-            family = "binomial", 
-            data = C34grassDF)
+m1 <- glmer(yv ~ year * co2 +  (1|block) +(1|ring) + (1|id), 
+            family = "binomial", data = C34grassDF)
 summary(m1)
+Anova(m1)
 plot(m1)
-# deviance >> df.resid, so it's highly overdispersed..
+# deviance >> df.resid, so it's highly overdispersed.. but P value is really 
+# really low so it would probably be still significant even if I take
+# overdispersion into account
 
 m2 <- glmer(yv ~ year * co2
             + (1|block) +  (1|ring) + (1|id) + (1|ResID), 
@@ -55,42 +53,30 @@ plot(m2)
 
 C34grassDF$C3Pr <- with(C34grassDF, c3/(c3 + c4))
 
-par(mfrow = c(1, 2))
-boxplot(C3Pr ~ year:ring, data = C34grassDF)
-boxplot(asin(C3Pr) ~ year:ring, data = C34grassDF)
+par(mfrow = c(1, 3))
+boxplot(C3Pr ~ year:ring, data = C34grassDF, main = "raw")
+boxplot(asin(C3Pr) ~ year:ring, data = C34grassDF, main = "arcsin")
+boxplot(logit(C3Pr) ~ year:ring, data = C34grassDF, main = "logit")
+  # logit might be slightly better
 
-pm1 <- lmer(C3Pr ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = C34grassDF)
+pm1 <- lmer(logit(C3Pr) ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = C34grassDF)
 summary(pm1)
 Anova(pm1)
 Anova(pm1, test.statistic = "F")
 plot(pm1)
 # little bit wedge patter
-
-# arc sin transformation----
-pm2 <- lmer(asin(C3Pr) ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = C34grassDF)
-summary(pm2)
-Anova(pm2)
-Anova(pm2, test.statistic = "F")
+qqnorm(resid(pm1))
+qqline(resid(pm1))
+# there one off the line. what if I remove
+which(qqnorm(resid(pm1))$y == min(qqnorm(resid(pm1))$y))
+pm2 <- lmer(logit(C3Pr) ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = C34grassDF[-1, ])
 plot(pm2)
-# slightly improved...
 qqnorm(resid(pm2))
-qqline(resid(pm2))
+qqline(resid(pm1))
+Anova(pm2, test.statistic = "F")
+# improved a lot. but needs to inspect more
 
-plot(allEffects(pm2))
-
-# logit transformation----
-# arc sin transformation
-pm3 <- lmer(logit(C3Pr, adjust = 0.05) ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = C34grassDF)
-summary(pm3)
-Anova(pm3)
-Anova(pm3, test.statistic = "F")
-plot(pm3)
-plot(allEffects(pm3))
-# slightly improved...
-qqnorm(resid(pm3))
-qqline(resid(pm3))
-
-# try different adjust values
+# try different adjust values for logit transformation
 logFun <- function(x){
   pm3 <- lmer(logit(C3Pr, adjust = x) ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = C34grassDF)
   qqnorm(resid(pm3), main = x)
@@ -98,29 +84,17 @@ logFun <- function(x){
 }
 par(mfrow = c(3, 3))
 sapply(seq(0.02, 0.06, length = 9), logFun)
-# adjust = 0.05 looks good
+# not much difference
 
 # contrast----
 
 # contrast doesn't work with lmer. so use lme
-lmeMod <- lme(logit(C3Pr, adjust = 0.05) ~ year * co2, random = ~1|block/ring/id, data = C34grassDF)
-
+tdf <- C34grassDF[-1, ]
+lmeMod <- lme(logit(C3Pr) ~ year * co2, random = ~1|block/ring/id, data = tdf)
 cntrst<- contrast(lmeMod, 
-                  a = list(year = levels(C34grassDF$year), co2 = "amb"),
-                  b = list(year = levels(C34grassDF$year), co2 = "elev"))
-cntrst
+                  a = list(year = levels(tdf$year), co2 = "amb"),
+                  b = list(year = levels(tdf$year), co2 = "elev"))
+PropC3_CntrstRes <- cntrstTbl(cntrst, data = tdf, variable = "C3Prop")
+PropC3_CntrstRes
 # no significant differece between treatments, but still c3 proportion decreased
 # at amb
-
-cdf <- confint(pm3, method = "boot")
-?Effect
-cdf
-sumdf <- summary(pm3)
-sumdf$coefficients
-predict(pm3, newdata = expand.grid(year = c("2012", "2014")))
-
-
-
-
-
-str(sumdf)
