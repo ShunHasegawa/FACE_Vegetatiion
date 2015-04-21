8bee3e5ad6e0df51048857b18450861218dc216c
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-04-21 19:25:56 +0100

    three year data sets are combined

--- R/CombineYearlyData.R
+++ R/CombineYearlyData.R
@@ -2,26 +2,149 @@
 # Combine 2013, 2014 2015 Data #
 ################################
 
-# 2013 data
+# 2013
 # source("R/veg.12.process.R")
 load("output/Data/FACE_Vegetation_2013.RData")
 
-# process 2014
+# 2014
 # source("R/prcss.veg.2014.R")
 load("output/Data/FACE_Veg2014.RData")
 
-veg.face <- rbind.fill(df2013, veg.14)
+# 2015
+# source("R/prcss.veg.2015.R")
+load("output//Data/FAVE_vegetation2015.RData")
+
+vdf <- rbind.fill(df2013, veg.14, veg.2015)
 
 ####################
 # organise dataset #
 ####################
 # turn na into 0
-veg.face[is.na(veg.face)] <- 0
+vdf[is.na(vdf)] <- 0
+
+# remove spp which were not observed
+SiteVec <- c("year", "ring", "plot", "position", "cell")
+SppVec <- names(vdf)[!names(vdf) %in% SiteVec]
+all(colSums(vdf[, SppVec]) > 0)
 
 # sort columns
-SiteVec <- c("year", "ring", "plot", "position", "cell")
-SppVec <- names(veg.face)[!names(veg.face) %in% SiteVec]
-veg.face <- veg.face[, c(SiteVec, sort(SppVec))]
+vdf <- vdf[, c(SiteVec, sort(SppVec))]
 
-## save
-save(veg.face, file = "output/Data/FACE_Vegetation_Raw_2013&2014.RData")
+# rmeove unknown spp
+vdf <- vdf[!grepl("unknown", names(vdf), ignore.case = TRUE)]
+
+# organise spp----
+SpName <- names(vdf)[!names(vdf) %in% SiteVec]
+
+# Carex.breviformis -> Carex.breviculmis
+vdf <- OrgSpp(vdf, siteVec = SiteVec,  
+              KeepCol = "Carex.breviculmis", 
+              CombineCol = SpName[grepl("carex", SpName, ignore.case = TRUE)])
+
+# No too sure about Cyperus, but they're small abundance so just combine
+colSums(vdf[, SpName[grepl("Cyperus", SpName, ignore.case = TRUE)]])
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Cyperus.flaccidus", 
+              CombineCol = SpName[grepl("Cyperus", SpName, ignore.case = TRUE)])
+
+# Eragrostis brownii and benthamii: Therse spp are treated as the same spp in 
+# Flora of the Sydney Region:
+# http://ausgrass2.myspecies.info/content/eragrostis-brownii
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Eragrostis.brownii", 
+              CombineCol = SpName[grepl("Eragrostis.b", SpName, ignore.case = TRUE)])
+
+# Fimbristylissp -> Fimbristylis.dichotoma 
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Fimbristylis.dichotoma", 
+              CombineCol = SpName[grepl("Fimbristylis", SpName, ignore.case = TRUE)])
+
+# Galium.sp -> Galium.propinquum
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Galium.propinquum", 
+              CombineCol = SpName[grepl("Galium", SpName, ignore.case = TRUE)])
+
+# Glycine is really hard to identify so just combine into Glycine.sp
+colSums(vdf[SpName[grepl("Glycine", SpName, ignore.case = TRUE)]])
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Glycine.sp", 
+              CombineCol = SpName[grepl("Glycine", SpName, ignore.case = TRUE)])
+
+# Oplisimenus.sp -> Oplismenus.aemulus
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Oplismenus.aemulus", 
+              CombineCol = SpName[grepl("Oplis", SpName, ignore.case = TRUE)])
+
+# Paspalum -> Paspalum.dilatatum
+vdf <- OrgSpp(vdf, siteVec = SiteVec,
+              KeepCol = "Paspalum.dilatatum", 
+              CombineCol = SpName[grepl("Paspalum", SpName, ignore.case = TRUE)])
+
+# Phyllanthussp -> Phyllanthus.sp 
+vdf <- OrgSpp(vdf, siteVec = SiteVec,  
+              KeepCol = "Phyllanthus.sp", 
+              CombineCol = SpName[grepl("Phyllanthus", SpName, ignore.case = TRUE)])
+
+# Poranthera.microphylla -> Poranthera.microphylla
+vdf <- OrgSpp(vdf, siteVec = SiteVec,  
+              KeepCol = "Poranthera.microphylla", 
+              CombineCol = SpName[grepl("Poranthera", SpName, ignore.case = TRUE)])
+
+# Rubussp -> Rubus.parvifolius
+vdf <- OrgSpp(vdf, siteVec = SiteVec,  
+              KeepCol = "Rubus.parvifolius", 
+              CombineCol = SpName[grepl("Rubus", SpName, ignore.case = TRUE)])
+
+# Schoenus.opogon -> Schoenus.apogon
+vdf <- OrgSpp(vdf, siteVec = SiteVec,  
+              KeepCol = "Schoenus.apogon", 
+              CombineCol = SpName[grepl("Schoenus", SpName, ignore.case = TRUE)])
+
+# Solanum.sp. -> Solanum.nigrum
+vdf <- OrgSpp(vdf, siteVec = SiteVec,  
+              KeepCol = "Solanum.nigrum", 
+              CombineCol = SpName[grepl("Solanum", SpName, ignore.case = TRUE)])
+names(vdf)
+
+# check all values <2
+all(vdf[, c(-1:-5)] < 2)
+# FALSE
+which(vdf[, c(-1:-5)] > 1, arr.ind = TRUE)
+names(vdf[, c(-1:-5)])[43]
+names(vdf[, c(-1:-5)])[34]
+# Glycine sp. Turn those into 1
+vdf[, c(-1:-5)][which(vdf[, c(-1:-5)] > 1, arr.ind = TRUE)] <- 1
+all(vdf[, c(-1:-5)] < 2)
+# TRUE
+
+# Spp list----
+vdf.mlt <- melt(vdf, id = c("year", "ring", "plot", "position", "cell"))
+spp <- data.frame(sp = sort(levels(vdf.mlt$variable)))
+write.csv(spp, file = "output/Data/spp_2015.csv", row.names = FALSE)
+
+# Spp which were found only in 2015
+Spp <- names(vdf)[!names(vdf) %in% SiteVec]
+YearSum <- ddply(vdf, .(year), function(x) colSums(x[Spp]))
+newSp <- names(YearSum)[apply(rbind(YearSum[1:2, ] == 0, YearSum[3, ] != 0), 2, all)]
+YearSum[newSp] # very small..
+
+# remove Lichen and Carex.breviformis for the time being----
+vdf$Lichen <- NULL
+
+# year is factor
+vdf$year <- factor(vdf$year)
+
+# save----
+save(vdf, file = "output//Data/FACE_Vegetation_Raw_2013_2015.RData")
+
+##############################
+# Create df including plant  #
+# characteristis (e.g. PFGs) #
+##############################
+vdf.mlt <- melt(vdf, id = c("year", "ring", "plot", "position", "cell"))
+
+# plant properties
+spList <- read.csv("Data//FACE_Vegetation_sp.list.csv", na.strings = c("NA", ""))
+VegRes15 <- merge(vdf.mlt, spList, by.x = "variable", by.y = "sp", all.x = TRUE)
+
+save(VegRes15, file = "output/Data/FACE_Vegetation_PFG_2015.RData")
