7b69445434cbcab500f967e146487757b7e96481
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-06-13 17:57:01 +0100

    C3 proportion in the whole community is added

--- R/Stats_C3_4_Grass.R
+++ R/Stats_C3_4_Grass.R
@@ -100,6 +100,69 @@ PropC3_CntrstRes
 # no significant differece between treatments, but still c3 proportion decreased
 # at amb
 
+########################################
+# C3 proportion in the whole community #
+########################################
+head(veg)
+
+# remove c3_4
+veg2 <- subsetD(veg, PFG != "c3_4")
+
+# get C3 proportion fo each plot
+PropDF <- ddply(veg2, .(year, co2, block, ring, plot, id), function(x){
+  C3prop = 1-sum(x$value[x$PFG == "c4"])/sum(x$value)
+  Total = sum(x$value)
+  data.frame(C3prop, Total)
+  })
+
+# glm
+m1 <- glmer(C3prop ~ year * co2 +  (1|block) +(1|ring) + (1|id), 
+            family = "binomial", data = PropDF, weight = Total)
+summary(m1)
+plot(m1)
+# overdispersed so try transformation with LMM
+
+# LMM
+bxplts(value = "C3prop", xval = "co2", data = PropDF)
+bxplts(value = "C3prop", xval = "ring", data = PropDF)
+
+par(mfrow = c(1, 3))
+boxplot(C3prop ~ year:ring, data = PropDF, main = "raw")
+boxplot(asin(C3prop) ~ year:ring, data = PropDF, main = "arcsin")
+boxplot(logit(C3prop) ~ year:ring, data = PropDF, main = "logit")
+# try logit
+
+rm1 <- lmer(logit(C3prop) ~ year * co2 + (1|block) +  (1|ring) + (1|id), data = PropDF)
+summary(rm1)
+Anova(rm1)
+Anova(rm1, test.statistic = "F")
+plot(rm1)
+# slightly wedged pattern
+qqnorm(resid(rm1))
+qqline(resid(rm1))
+
+# one complete outlier, so remove them
+rmv <- which(qqnorm(resid(rm1))$y %in% min(qqnorm(resid(rm1))$y))
+rm2 <- update(rm1, subset = -rmv)
+plot(rm2)
+qqnorm(resid(rm2))
+qqline(resid(rm2))
+
+# improved a lot
+AnvF_PropC3_total <- Anova(rm2, test.statistic = "F")
+AnvF_PropC3_total
+
+# contrast
+# contrast doesn't work with lmer. so use lme
+tdf <- PropDF[-rmv, ]
+lmeMod <- lme(logit(C3prop) ~ year * co2, random = ~1|block/ring/id, data = tdf)
+cntrst<- contrast(lmeMod, 
+                  a = list(year = levels(tdf$year), co2 = "amb"),
+                  b = list(year = levels(tdf$year), co2 = "elev"))
+PropC3_Total_CntrstRes <- cntrstTbl(cntrst, data = tdf, variable = "C3Prop")
+PropC3_Total_CntrstRes
+# no significant difference between treatment for each year
+
 ## ---- Stats_C34PropSmmry
 # The model
 pm2@call
@@ -116,4 +179,22 @@ PropC3_CntrstRes
 # Model diagnosis
 plot(pm2)
 qqnorm(resid(pm2))
-qqline(resid(pm1))
+qqline(resid(pm2))
+
+## ---- Stats_C3_TotalPropSmmry
+# The model
+rm2@call
+
+# Chisq
+Anova(rm2)
+
+# F test
+AnvF_PropC3_total
+
+# Contrast
+PropC3_Total_CntrstRes
+
+# Model diagnosis
+plot(rm2)
+qqnorm(resid(rm2))
+qqline(resid(rm2))
