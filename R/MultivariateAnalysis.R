eeb091701b488302c69639ed0105548a932295e9
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-07-08 08:52:04 +0100

    Euclidean dissimilarity is used

--- R/MultivariateAnalysis.R
+++ R/MultivariateAnalysis.R
@@ -6,137 +6,22 @@ head(PlotSumVeg)
 
 # compute dissimilarity for each plot
 disDF <- ddply(PlotSumVeg, .(block, ring, co2, plot), function(x) YearDssmlrty(x, spp = SppName))
+disDF$id <- disDF$ring:disDF$plot
 
 ################
 ## CO2 x Year ##
 ################
 
 # perform LMM----
-boxplot(BC ~ ring:year, data = disDF)
-bxplts(value = "BC", xval = "co2", data = disDF)
-bxplts(value = "BC", xval = "ring", data = disDF)
-  # log seems better
-
-disDF$id <- disDF$ring:disDF$plot
-m1 <- lmer(log(BC) ~ co2 * year + (1|block) + (1|ring) + (1|id), data = disDF)
-m2 <- lmer(log(BC) ~ co2 + year + (1|block) + (1|ring) + (1|id), data = disDF)
-anova(m1, m2)
-summary(m2)
-Anova(m2)
-DisAllSp_AnvF <- Anova(m2, test.statistic = "F")
-plot(m2)
-qqnorm(resid(m2))
-qqline(resid(m2))
-# one obvious outlier. what if I remove
-which(qqnorm(resid(m2))$x == max(qqnorm(resid(m2))$x))
-m3 <- lmer(log(BC) ~ co2 * year + (1|block) + (1|ring) + (1|id), data = disDF[-32, ])
-plot(m3)
-qqnorm(resid(m3))
-qqline(resid(m3))
-# improved
-summary(m3)
-Anova(m3)
-# result is not hugely different to the above. so just stay with the above
-
-############
-## ANCOVA ##
-############
-
-# fit soil moisture data---
-
-# load soil moisture data
-load("Data/FACE_TDR_ProbeDF.RData")
-summary(FACE_TDR_ProbeDF)
-soilDF <- subsetD(FACE_TDR_ProbeDF, Sample == "vegetation" & 
-                    Date >= as.Date("2013-1-1") &
-                    Date <= as.Date("2014-12-31"))
-
-soilDF <- within(soilDF, {
-  plot <- factor(plot)
-  year <- factor(ifelse(year(Date) == "2013", "Year1", "Year2"))
-})
-
-MoistTempDF <- ddply(soilDF, .(year, ring, plot), summarise, 
-                     Moist = mean(Moist, na.rm = TRUE),
-                     Temp = mean(Temp_Mean, na.rm = TRUE))
-boxplot(Moist ~ year:ring, data = MoistTempDF)
-
-# merge data frame
-vsDF <- merge(disDF, MoistTempDF, by = c("ring", "plot", "year"))
-vsDF$id <- with(vsDF, ring:plot)
-
-# fit to the model
-theme_set(theme_bw())
-
-# against Moist----
+bxplts(value = "EU", xval = "co2", data = disDF)
+bxplts(value = "EU", xval = "ring", data = disDF)
+  
 # raw data
-p <- ggplot(data = vsDF, aes(x = Moist, y = BC, col = co2, shape = year))
-p2 <- p + geom_point(size = 4) + geom_smooth(aes(fill = co2), method  = "lm") 
-p2
-# log
-p <- ggplot(data = vsDF, aes(x = log(Moist), y = log(BC), col = co2, shape = year))
-p2 <- p + geom_point(size = 4) + geom_smooth(aes(fill = co2), method  = "lm") 
-p2
-
-# against Temp----
-# raw
-p <- ggplot(data = vsDF, aes(x = Temp, y = BC, col = co2, shape = year))
-p2 <- p + geom_point(size = 4) + geom_smooth(aes(fill = co2), method  = "lm") 
-p2
-# log
-p <- ggplot(data = vsDF, aes(x = log(Temp), y = log(BC), col = co2, shape = year))
-p2 <- p + geom_point(size = 4) + geom_smooth(aes(fill = co2), method  = "lm") 
-p2
-# not much difference in temperature between years
-
-# There's small indication of positive correlation in Year2 adn negative
-# correlation in Year1.
-# Analyse each year separately----
-vsDF$logMoist <- log(vsDF$Moist)
-mls <- dlply(vsDF, .(year), function(x) {
-  tdf <- x
-  lmer(log(BC) ~ co2 + logMoist + (1|block) + (1|ring), data = tdf)
-})
-llply(mls, Anova)
-# moisture effects are not significant when analysed separately, so do not worry
-# about yearly difference in correlation for the time being
-
-################
-## Fit models ##
-################
-# log----
-m1 <- lmer(log(BC) ~ co2 * logMoist + (1|block) + (1|ring) + (1|id), data = vsDF)
-Anova(m1)
-m2 <- update(m1, ~. - co2:logMoist)
-Anova(m2)
-Anova(m2, test.statistic = "F")
-
-# 2nd polynomial----
-m3 <- lmer(BC ~ co2 * (Moist + I(Moist^2)) + (1|block) + (1|ring) + (1|id), data = vsDF)
-Anova(m3)
-m4 <- lmer(BC ~ co2 + Moist + I(Moist^2) + (1|block) + (1|ring) + (1|id), data = vsDF)
-Anova(m4)
-Anova(m4, test.statistic = "F")
-
-# compare two models
-ListMl <- list(m2, m4)
-names(ListMl) <- c("log", "2nd Polynomial")
-
-# model diagnosis
-plot(m2)
-plot(m4)
-par(mfrow = c(1, 2))
-l_ply(names(ListMl), function(x) {
-  l <- ListMl[[x]]
-  qqnorm(resid(l), main = x)
-  qqline(resid(l))
-})
-ldply(ListMl, r.squared)
-
-visreg(m2, xvar = "logMoist", xtrans = exp, trans = exp, by = "co2", overlay = TRUE)
-visreg(m4, xvar = "Moist", by = "co2", overlay = TRUE)
-# no huge difference but log seems slightly better so use log
-
+m1 <- lmer(EU ~ co2 * year + (1|block) + (1|ring) + (1|id), data = disDF)
+DisAllSp_AnvF <- Anova(m1, test.statistic = "F")
+plot(m1)
+qqnorm(resid(m1))
+qqline(resid(m1))
 
 ###########################
 # Plant Functional Groups #
@@ -150,107 +35,20 @@ Pfg_disDF <- ddply(PlotSumPFGMatrix, .(block, ring, co2, plot),
                function(x) YearDssmlrty(x, spp = PFGName))
 Pfg_disDF$id <- with(Pfg_disDF, ring:plot)
 
-# combine with moisture data
-summary(MoistTempDF)
-PFG_vsDF <- merge(Pfg_disDF, MoistTempDF, by = c("ring", "plot", "year"))
-
-theme_set(theme_bw())
-p <- ggplot(data = PFG_vsDF, aes(x = log(Moist), y = log(BC), col = co2, shape = year))
-p2 <- p + geom_point(size = 4) + geom_smooth(method = "lm")
-p2
-# it seems that BC goes up till moisture of 0.1, then goes down. may be
-# secon-plynomial might be appropriate
-p <- ggplot(data = PFG_vsDF, aes(x = Moist, y = BC, col = co2))
-p2 <- p + geom_point(size = 4) + 
-  geom_smooth(method = "lm", formula = y ~ poly(x, 2, raw = TRUE))
-p2
-
-############################
-## what if I include year ##
-############################
-PFG_vsDF$logMoist <- log(PFG_vsDF$Moist)
-m1 <- lmer(log(BC) ~ co2 * year * logMoist + (1|block) + (1|ring) + (1|id), data = PFG_vsDF)
-m2 <- stepLmer(m1)
-Anova(m2)
-DisPfg_AnvF <- Anova(m2, test.statistic = "F")
-plot(allEffects(m2))
-# no moisture effect. Large proportion of variability is explained by moisture
-
-# each year----
-Mlist <- dlply(PFG_vsDF, .(year), function(x) {
-  dfs <- x
-  lmer(log(BC) ~ co2 + logMoist + (1|block) + (1|ring), data = x)
-  })
-llply(Mlist, Anova)
-llply(Mlist, summary)
-# negative moisture effect only in the first year... this is contradictive to
-# the overall trend (i.e., it was really dry in the Year2 and dissimilarity was
-# quite low compared to Year1, so overall treand of dissimilarity against
-# moisture is positive)--> need more inspection 
+bxplts(value = "EU", xval = "ring", data = Pfg_disDF)
+bxplts(value = "EU", xval = "co2", data = Pfg_disDF)
 
 #################
 ## perform LMM ##
 #################
-
-# fit log and 2nd polynomial
-
-# log-log----
-Pfg_ll1 <- lmer(log(BC) ~ co2 * log(Moist) + (1|block) + (1|ring) + (1|id), data = PFG_vsDF)
-Anova(Pfg_ll1)
-Anova(Pfg_ll1, test.statistic = "F")
-
-# 2nd polynomial----
-Pfg_pl1 <- lmer(BC ~ co2 * (Moist + I(Moist^2)) + (1|block) + (1|ring) + (1|id), data = PFG_vsDF)
-Pfg_pl2 <- update(Pfg_pl1, ~. - co2:I(Moist^2))
-anova(Pfg_pl1, Pfg_pl2)
-Anova(Pfg_pl2)
-
-# model diagnosis----
-ListMl <- list(Pfg_ll1, Pfg_pl2)
-names(ListMl) <- c("log-log", "2nd polynomial")
-
-plot(Pfg_ll1)
-plot(Pfg_pl2)
-  # indication of curvature..
-par(mfrow = c(1, 2))
-l_ply(names(ListMl), function(x) {
-  l <- ListMl[[x]]
-  qqnorm(resid(l), main = x)
-  qqline(resid(l))
-})
-# squared R
-ldply(ListMl, r.squared)
-llply(ListMl, summary)
-
-par(mfrow = c(1, 2))
-visreg()
-
-# although r.squared is slightly higher for 2nd plynomial, the number of 
-# parameters is also bigger (5 vs. 4). Also, qqplot looks slightly better (?)
-# for log-log. So use log.
-visreg(Pfg_ll1, xvar = "Moist", trans = exp, by = "co2", overlay = TRUE)
-visreg(Pfg_pl2, xvar = "Moist", by = "co2", overlay = TRUE)
-
-# create a plot with confidense intervals----
-
-# DF for predicted values
-range(PFG_vsDF$Moist)
-expDF <- expand.grid(co2 = c("amb", "elev"), Moist = seq(0.03, 0.2, length.out = 50))
-bb <- bootMer(Pfg_ll1, FUN = function(x) predict(x, expDF, re.form = NA), nsim=500)
-lci <- exp(apply(bb$t, 2, quantile, 0.025))
-uci <- exp(apply(bb$t, 2, quantile, 0.975))
-BC <- exp(bb$t0)
-predDF <- cbind(lci, uci, BC, expDF)
-
-p <- ggplot(data = PFG_vsDF, aes(x = log(Moist), y = log(BC), col = co2, fill = co2))
-p2 <- p + geom_point(size = 3) + 
-  geom_line(aes(x = log(Moist), y = log(BC)), data = predDF) +
-  geom_ribbon(aes(ymin = log(lci), ymax = log(uci), x = log(Moist)), 
-              alpha = .2, color = NA, data = predDF) +
-  scale_color_manual(values = c("blue", "red")) +
-  scale_fill_manual(values = c("blue", "red"))
-p2
-ggsavePP(filename = "output//figs/PFG_DissimPredVal_Moist", plot = p2, width = 6, height = 4)
+# raw data
+m1 <- lmer(EU ~ co2 * year + (1|block) + (1|ring) + (1|id), data = Pfg_disDF)
+Anova(m1)
+m2 <- stepLmer(m1)
+DisPfg_AnvF <- Anova(m2, test.statistic = "F")
+plot(m2)
+qqnorm(resid(m2))
+qqline(resid(m2))
 
 ##########
 # Figure #
@@ -260,13 +58,13 @@ disDF$type <- "All species"
 PFG_vsDF$type <- "PFG"
 
 disDF_Ring <- ldply(list(disDF, PFG_vsDF), function(x) {
-  ddply(x, .(type, year, co2, ring), summarise, BC = mean(BC))
+  ddply(x, .(type, year, block, co2, ring), summarise, EU = mean(EU))
 })
 
 disDF_CO2 <- ddply(disDF_Ring, .(type, year, co2), summarise, 
-                   Mean = mean(BC), 
-                   SE = ci(BC)[4], 
-                   N = sum(!is.na(BC)))
+                   Mean = mean(EU), 
+                   SE = ci(EU)[4], 
+                   N = sum(!is.na(EU)))
 
 # plot----
 PsDdg <- .3
@@ -276,11 +74,56 @@ p2 <- p +
                 position = position_dodge(PsDdg), 
                 width = 0) +
   geom_point(size = 3, position = position_dodge(PsDdg), shape = 21) +
-  scale_fill_manual(values = c("white", "black"), labels = c("Ambient", expression(eCO[2]))) + 
-  labs(x = "Year", y = "Bray–Curtis dissimilarity") + 
+  scale_fill_manual(values = c("black", "white"), labels = c("Ambient", expression(eCO[2]))) + 
+  labs(x = "Year", y = "Euclidean dissimilarity") + 
   facet_wrap(~ type, scale = "free_y") + 
   science_theme + 
   theme(legend.position = c(.85, .85))
-ggsavePP(filename = "output/figs/FACE_BC_Dissimilarity_CO2", plot = p2,
+ggsavePP(filename = "output/figs/FACE_EU_Dissimilarity_CO2", plot = p2,
          width = 4, height = 2.5)
 
+
+#######################
+# CO2 response ratios #
+#######################
+disDF_Ring
+
+# cast to get each block ratio
+disDF_Ring_cst <- dcast(disDF_Ring, type + year + block ~ co2, value.var = "EU")
+disDF_Ring_cst$co2R <- disDF_Ring_cst$elev/disDF_Ring_cst$amb - 1
+
+# compute SE for ratio using non-parametric bootstrap
+ratio <- function(d, w) sum(d$elev * w)/sum(d$amb * w) - 1
+RatioSE <- ddply(disDF_Ring_cst, .(type, year), function(x) {
+  b <- boot::boot(x, ratio, R = 999, stype = "w")
+  summary(b)
+})
+
+# plot
+tdf <- within(RatioSE, {co2R = original})
+
+theme_set(theme_bw())
+p <- ggplot(tdf, aes(x = year, y = co2R))
+p2 <- p + geom_point(size = 3) + 
+  geom_errorbar(aes(x = year, ymin = co2R - bootSE, ymax = co2R + bootSE), width = 0) +
+  geom_point(data = disDF_Ring_cst, aes(x = year, y = co2R), col = "red", alpha = .7, size = 2)+
+  geom_hline(yintercept = 0, linetype = "dashed") +
+  facet_wrap( ~ type, scale = "free_y", ncol = 5) +
+  labs(y = "CO2 response ratio", x = "") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        strip.text.x = element_text(size = 7))
+p2
+ggsavePP(plot = p2, filename = "output/figs/FACE_CO2ResponseRatio_Dissimilarity", width = 4, height = 3)
+
+# organise it to export as a table
+RatioSE$co2R <- with(RatioSE, paste0(round(original, 2), "(", round(bootSE, 2), ")"))
+
+# reorder
+RatioSE_cst <- dcast(RatioSE, type ~ year, value.var = "co2R")
+write.csv(RatioSE_domspp_cst, file = "output/table/CO2ResponseRatio_Dissimilarity.csv", row.names = FALSE)
+
+###########
+# Summary #
+###########
+DisAllSp_AnvF
+DisPfg_AnvF
