6adc73820fc9e96240b8257f5388b4834e9a480c
Shun Hasegawa <hasegawashun.virgo@googlemail.com>
2016-03-18 12:50:52 +1100

    After updating packages StatEllipsed stopped working

--- R/functions.R
+++ R/functions.R
@@ -1,41 +1,41 @@
-#############################
-# create ellipses on ggplot #
-#############################
-StatEllipse <- proto(ggplot2:::Stat,
-{
-  required_aes <- c("x", "y")
-  default_geom <- function(.) GeomPath
-  objname <- "ellipse"
-  
-  calculate_groups <- function(., data, scales, ...){
-    .super$calculate_groups(., data, scales,...)
-  }
-  calculate <- function(., data, scales, level = 0.75, segments = 51,...){
-    dfn <- 2
-    dfd <- length(data$x) - 1
-    if (dfd < 3){
-      ellipse <- rbind(c(NA,NA))	
-    } else {
-#       require(MASS)
-      v <- cov.trob(cbind(data$x, data$y))
-      shape <- v$cov
-      center <- v$center
-      radius <- sqrt(dfn * qf(level, dfn, dfd))
-      angles <- (0:segments) * 2 * pi/segments
-      unit.circle <- cbind(cos(angles), sin(angles))
-      ellipse <- t(center + radius * t(unit.circle %*% chol(shape)))
-    }
-    
-    ellipse <- as.data.frame(ellipse)
-    colnames(ellipse) <- c("x","y")
-    return(ellipse)
-  }
-}
-)
-
-stat_ellipse <- function(mapping=NULL, data=NULL, geom="path", position="identity", ...) {
-  StatEllipse$new(mapping=mapping, data=data, geom=geom, position=position, ...)
-}
+# #############################
+# # create ellipses on ggplot #
+# #############################
+# StatEllipse <- proto(ggplot2:::Stat,
+# {
+#   required_aes <- c("x", "y")
+#   default_geom <- function(.) GeomPath
+#   objname <- "ellipse"
+#   
+#   calculate_groups <- function(., data, scales, ...){
+#     .super$calculate_groups(., data, scales,...)
+#   }
+#   calculate <- function(., data, scales, level = 0.75, segments = 51,...){
+#     dfn <- 2
+#     dfd <- length(data$x) - 1
+#     if (dfd < 3){
+#       ellipse <- rbind(c(NA,NA))	
+#     } else {
+# #       require(MASS)
+#       v <- cov.trob(cbind(data$x, data$y))
+#       shape <- v$cov
+#       center <- v$center
+#       radius <- sqrt(dfn * qf(level, dfn, dfd))
+#       angles <- (0:segments) * 2 * pi/segments
+#       unit.circle <- cbind(cos(angles), sin(angles))
+#       ellipse <- t(center + radius * t(unit.circle %*% chol(shape)))
+#     }
+#     
+#     ellipse <- as.data.frame(ellipse)
+#     colnames(ellipse) <- c("x","y")
+#     return(ellipse)
+#   }
+# }
+# )
+# 
+# stat_ellipse <- function(mapping=NULL, data=NULL, geom="path", position="identity", ...) {
+#   StatEllipse$new(mapping=mapping, data=data, geom=geom, position=position, ...)
+# }
 
 ###################
 # plot CA results #
@@ -289,9 +289,12 @@ YearDssmlrty <- function(x, spp) {
   df2 <- subset(x, year %in% c("Year1", "Year2"))
   dis2_bc <- vegdist(log(df2[, spp] + 1), method = "bray")
   dis2_eu <- vegdist(log(df2[, spp] + 1), method = "euclidean")
-  dfs <- data.frame(year = c("Year0-1", "Year1-2"), 
-                    BC = c(dis1_bc, dis2_bc), 
-                    EU  = c(dis1_eu, dis2_eu))
+  df3 <- subset(x, year %in% c("Year2", "Year3"))
+  dis3_bc <- vegdist(log(df3[, spp] + 1), method = "bray")
+  dis3_eu <- vegdist(log(df3[, spp] + 1), method = "euclidean")
+  dfs <- data.frame(year = c("Year0-1", "Year1-2", "Year2-3"), 
+                    BC = c(dis1_bc, dis2_bc, dis3_bc), 
+                    EU  = c(dis1_eu, dis2_eu, dis3_eu))
   return(dfs)
 }
 
