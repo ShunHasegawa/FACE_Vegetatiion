2681253d12cc57f76583708ad42f42330d58490b
Shun Hasegawa <hasegawashun.virgo@googlemail.com>
2016-08-03 11:35:26 +1000

    unused sections are removed

--- R/Stats.R
+++ R/Stats.R
@@ -2,45 +2,21 @@
 ## Stats ##
 ###########
 
-######
-# CA #
-######
-# source("R/CA.analysis.R")
-
 #######
 # RDA #
 #######
 # source("R/RDA.analysis.R")
 source("R/FitEnvironmentalVars.R")
 
-########
-# CAP  #
-########
-# source("R/CAP.analysis.R")
-
-#######
-# GLM #
-#######
-
 ####################
 # Dominent species #
 ####################
 source("R/Stats_DominantSpp.R")
 
-#########
-# C3:C4 #
-#########
+##################
+# PFG proportion #
+##################
 source("R/Stats_C3_4_Grass.R")
-
-####################
-# Legume:nonLegume #
-####################
-# source("R/Stats_Legume.R")
-
-#########################
-# native vs. introduced #
-#########################
-# source("R/Stat_origin.R")
 
 ################### 
 # Diversity index #
