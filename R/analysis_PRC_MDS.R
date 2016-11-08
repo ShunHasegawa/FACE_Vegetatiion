e121dc24b283e40f2c37b94c718b5901bc1fa949
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2016-11-08 18:47:38 +1030

    multiplot function to merge multiple ggplots was added

--- R/analysis_PRC_MDS.R
+++ R/analysis_PRC_MDS.R
@@ -8,9 +8,6 @@ prc_site <- select(PlotSumVeg, year, ring, plot, co2)        # site df
 
 # analysis ----------------------------------------------------------------
 
-# ## environmental variable
-# load("output/Data/EucFACE_understorey_env_vars_2012-2016.RData")            # load environmental variables, generated in "R/FitEnvironmentalVars.R"
-# identical(EnvDF_3df[, c("year", "ring")], RingSumVeg[, c("year", "ring")])  # check EnvDf_3df and species df are in the same order for site
 
 ## Principal response curve anlsyis with bray-curtis dissimilarity
 prc_all <- capscale(prc_sp ~  year * co2 + Condition(year), data = prc_site, 
@@ -18,12 +15,34 @@ prc_all <- capscale(prc_sp ~  year * co2 + Condition(year), data = prc_site,
 
 
 ## prepare df for a figure from the result of prc
-summary_prc       <- summary(prc_all, scaling = 3)       # use scaling = 3, it doesn't matter too much, but this is used in the original prc function
-res_prc_site      <- cbind(summary_prc$sites, prc_site)  # site score
-res_prc_site_ring <- res_prc_site %>%                    # site scores by ring
+summary_prc       <- summary(prc_all, scaling = 3)                            # use scaling = 3, it doesn't matter too much, but this is used in the original prc function
+MDSprop           <- summary_prc$cont$importance[2, c("MDS1", "MDS2")]        # proportion explained by the first 2 MDS axies
+MDSaxes           <- paste0("MDS", 1:2, " (", round(MDSprop * 100, 0), "%)")  # MDS axis with explained proportion
+res_prc_site      <- cbind(summary_prc$sites, prc_site)                       # site score
+res_prc_site_ring <- res_prc_site %>%                                         # site scores by ring
   group_by(year, ring, co2) %>% 
-  summarise_each(funs(mean), CAP1, MDS1, MDS2)           # CAP1 is for PRC plot and MDSs for visualisation
-res_prc_site_co2  <- res_prc_site_ring %>%               # canonical coefficients for CO2 treatment (i.e. treatment difference for each year)
+  summarise_each(funs(mean), CAP1, MDS1, MDS2) %>%                            # CAP1 is for PRC plot and MDSs for visualisation
+  arrange(ring, year)
+
+
+## fit envronmental variable
+load("output/Data/EucFACE_understorey_env_vars_2012-2016.RData")                  # load environmental variables, generated in "R/FitEnvironmentalVars.R"
+identical(EnvDF_3df[, c("year", "ring")],                                         # check EnvDf_3df and species df are in the same order for site
+          data.frame(res_prc_site_ring[, c("year", "ring")]))  
+cntr       <- how(within = Within(type = "series"),                               # define permutatoin. ring is exchangable; autocorrelation is taken into consideration by series
+                  plots = Plots(strata = res_prc_site_ring$ring, type = "free"),
+                  nperm = 4999)
+mds_envfit <- envfit(res_prc_site_ring[, c("MDS1", "MDS2")],                      # fit environmental variables
+                     EnvDF_3df[, c("Depth_HL", "TotalC", "Drysoil_ph", "moist")],
+                     permutations = cntr)
+mds_arrw_d <- data.frame(mds_envfit$vectors$arrows) %>%                           # arrows for environmental variables 
+  mutate(env = mapvalues(row.names(.), row.names(.), 
+                         c("Depth HL", "Total C", "pH", "Moist")),
+         co2 = factor("amb", levels = c("amb", "elev")),
+         year = factor("Year0", levels = paste0("Year", 0:3)))
+
+
+res_prc_site_co2  <- res_prc_site_ring %>%                                    # canonical coefficients for CO2 treatment (i.e. treatment difference for each year)
   group_by(year, co2) %>% 
   summarise_each(funs(mean), CAP1) %>% 
   group_by(year) %>% 
@@ -166,21 +185,34 @@ fig_prc_spp_byPfg
 
 # . PCoA (MDS) plot -------------------------------------------------------
 
+mds_arrw_d2 <- mds_arrw_d %>% 
+  mutate(MDS1 = MDS1 * c(.8, .6, .6, .6), 
+         MDS2 = MDS2 * c(.5, .6, .6, .6))
 
 fig_prc_mds <- ggplot(res_prc_site_ring, aes(x = MDS1, y = MDS2, shape = year, 
                                              fill = co2)) +
+  
+  labs(x = MDSaxes[1], y = MDSaxes[2]) +
   geom_hline(yintercept = 0, linetype = "dotted", col = "gray") +
   geom_vline(xintercept = 0, linetype = "dotted", col = "gray") +
   
-  geom_point(size = 2, alpha = .8) +
   geom_path(aes(group = ring, linetype = co2)) +
+  geom_point(size = 2, alpha = .6) +
+  
+  geom_segment(data = mds_arrw_d,
+               aes(x = 0, y = 0, xend = MDS1 * .55, yend = MDS2 * .55),
+               arrow = arrow(length = unit(.2, "cm")),  alpha = .7) +
+  geom_text(data = mds_arrw_d2, 
+            aes(x = MDS1, y = MDS2, label = env),  size = 2, 
+            fontface = "bold") +
+  
   
   science_theme +
   theme(legend.title = element_text(size = 10)) +
   scale_fill_manual(name   = expression(CO[2]),
                     values = c("black", "white"), 
                     labels = c("Ambient", expression(eCO[2])),
-                    guide  = guide_legend(override.aes = list(shape = 21))) +
+                    guide  = guide_legend(override.aes = list(shape = 21, alpha = 1))) +
   scale_shape_manual(name = "Year", 
                      values = c(21, 22, 23, 24)) +
   scale_linetype_manual(name = expression(CO[2]),
--- R/functions.R
+++ R/functions.R
@@ -648,6 +648,47 @@ science_theme <- theme(panel.border      = element_rect(color = "black"),
                        axis.text.y       = element_text(margin = margin(0, 5)),
                        axis.title.y      = element_text(margin = margin(0, 10)))
 
+
+# this function combines multiple ggplots
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+
+
+
 # correct year0 vaalue; this remove species that were observed only in Year0 yet
 # not in the subseqent years
 correct_year0 <- function(x, spp){
