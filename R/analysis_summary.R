308d50811154f8d5fdbb25c0a11106d1879cc19f
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2017-03-09 16:52:32 +1100

    summary tables for anova were regenerated for those that was to be in the main and appendix documents

--- R/analysis_summary.R
+++ R/analysis_summary.R
@@ -1,17 +1,25 @@
-# combine all models
+
+# combine all models ------------------------------------------------------
+
+
 all_m_list <- list('diversity'   = div_m_list,
                    'dominentSpp' = dom_m_list,
                    'grass_prop'  = grassprop_m,
                    'pfg_prop'    = pfgprop_m_list)
 all_m_list <- unlist(all_m_list)
 
-# combine and organise anova table
+
+
+
+# combine and organise anova table ----------------------------------------
+
+
 anova_df <- ldply(all_m_list, function(x) tidy(Anova(x, test.statistic = "F")))
 
 anova_df_ed <- anova_df %>% 
-  mutate(term  = factor(ifelse(term %in% c("co2", "year", "co2:year"), term, "BL"),
-                        levels = c("co2", "year", "co2:year", "BL"),
-                        labels = c("CO2", "Year", "CO2xYear", "BL")),
+  mutate(term  = factor(ifelse(term %in% c("co2", "year", "co2:year"), term, "Baseline"),
+                        levels = c("co2", "year", "co2:year", "Baseline"),
+                        labels = c("CO2", "Year", "CO2xYear", "Baseline")),
          statistic = round(statistic, 2),
          Df.res    = round(Df.res, 0), 
          Fv        = paste0("F(", df, ",", Df.res, ")=", statistic),
@@ -22,39 +30,59 @@ anova_df_ed <- anova_df %>%
   gather(variable, value, Fv, P)
 anova_df_ed
 
-# create tables
+
+
+
+# create tables -----------------------------------------------------------
+
+
 
 # diversity indices
 div_tbl <- anova_df_ed %>% 
   filter(Type == "diversity") %>% 
-  mutate(Form = tstrsplit(.id, "[.]")[[2]],
-         Ind  = tstrsplit(.id, "[.]")[[3]],
-         response = paste(Ind, Form, sep =  "_")) %>% 
-  select(Type, term, variable, value, response)
+  mutate(Form       = tstrsplit(.id, "[.]")[[2]],
+         Ind        = tstrsplit(.id, "[.]")[[3]],
+         response   = paste(Ind, Form, sep =  "_"),
+         report     = ifelse(Form == "grass_spp", "main", "appendix")) %>% 
+  select(Type, term, variable, value, response, report) 
+  
 
 # dominent species
 dom_tbl <- anova_df_ed %>% 
   filter(Type == "dominentSpp") %>%
-  mutate(response = paste(tstrsplit(.id, "[.]")[[2]], tstrsplit(.id, "[.]")[[3]])) %>% 
-  select(Type, term, variable, value, response)
+  mutate(response = paste(tstrsplit(.id, "[.]| ")[[2]], tstrsplit(.id, "[.]| ")[[3]]),
+         report   = "main") %>% 
+  select(Type, term, variable, value, response, report)
 
 # PFG proportion
 pfg_tbl <- anova_df_ed %>% 
   filter(Type %in% c("grass_prop", "pfg_prop")) %>% 
-  mutate(response = ifelse(.id == "grass_prop", "grass_prop", tstrsplit(.id, "[.]")[[2]])) %>% 
-  select(Type, term, variable, value, response) 
+  mutate(response = ifelse(.id == "grass_prop", "grass_prop", tstrsplit(.id, "[.]")[[2]]),
+         report   = ifelse(response == "C3vsC4", "main", "appendix")) %>% 
+  select(Type, term, variable, value, response, report) 
 
 # mege the above tables
 all_tbl <- rbind.fill(div_tbl, dom_tbl, pfg_tbl) %>% 
-  mutate(tv = paste(term, variable, sep = "_")) %>% 
+  mutate(tv       = paste(term, variable, sep = "_"),
+         response = dplyr::recode(response, C3vsC4 = 'C4 proportion')) %>% 
   select(-term, -variable) %>% 
   spread(tv, value) %>% 
-  select(-Type)
+  select(response, starts_with("Baseline"), starts_with("CO2_"), starts_with("Year"),
+         starts_with("CO2x"), report, -Type)
 all_tbl
 
 
+# split by report type (main or appendix document)
+all_tbl_l <- dlply(all_tbl, .(report), function(x) select(x, -report))
+
+
 # save tables
-write.csv(all_tbl, "output/table/summary_rptdAov_tbl.csv", row.names = FALSE)
+writeWorksheetToFile(file        = "output/table/summary_rptdAov_tbl.xlsx",    # define file name to be saved
+                     data        = all_tbl_l,                                  # writeWorksheetToFile doesn't take dplyr object so turn them into data frames using as.data.frame
+                     sheet       = names(all_tbl_l),                           # sheet names in excel are defined by object names a list
+                     clearSheets = TRUE)
+
+
 
 
 save.image(file = "output/Data/summary_analysis.RData")
