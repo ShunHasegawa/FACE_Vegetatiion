7644b068de7961f78157420d5e6b0c49b26f808c
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2017-04-12 21:24:42 +1000

    canpy transmittance and soil temperature were replaced with understorey par and air temperature

--- R/figure_env_vars.R
+++ R/figure_env_vars.R
@@ -2,40 +2,61 @@
 # Light -------------------------------------------------------------------
 
 # get daily mean
-load("output/Data/FACE_canopy_transmittance_raw.RData")
-light_dayly_co2 <- light_raw %>% 
-  mutate(co2 = factor(ifelse(Ring %in% c("R1", "R4", "R5"), "eCO2", "Ambient"))) %>% 
-  group_by(Date, co2) %>% 
-  summarise_each(funs(mean), Gapfraction.mean)
+load("output/Data/FACE_FloorPAR.RData")
+Lightdf$PAR <- rowMeans(Lightdf[, c("PAR_Den_1_Avg", "PAR_Den_2_Avg", "PAR_Den_3_Avg")], na.rm = TRUE)
 
-# define sampling period
-light_range <- data.frame(start_date = seq(as.Date("2012-10-26"), as.Date("2015-10-26"), "year"),
-                          end_date   = seq(as.Date("2012-12-31"), as.Date("2015-12-31"), "year"),
-                          ymin       = -Inf,
-                          ymax       = Inf,
-                          co2 = "Ambient")
+light_dayly_co2 <- Lightdf %>% 
+  filter(Date >= as.Date("2012-10-15")) %>% 
+  mutate(co2 = factor(ifelse(ring %in% c("1", "4", "5"), "eCO2", "Ambient"))) %>% 
+  group_by(Date, co2) %>% 
+  summarise(PAR = mean(PAR, na.rm = TRUE))
+
 
 # add survey dates
 suevey_date <- data.frame(Date = as.Date(c("2012-09-15", "2012-12-15", "2014-1-15", "2015-1-30", "2016-2-15")))
 
 
 fig_light <- ggplot() +
-  geom_rect(data = light_range, 
-            aes(xmin = start_date, 
-                xmax = end_date, 
-                ymin = -Inf, ymax = Inf), 
-            fill = "gray70", col = NA) +
-  geom_line(data = light_dayly_co2, aes(x = Date, y = Gapfraction.mean, col = co2)) +
-  geom_point(data = suevey_date, aes(x = Date, y = .35), shape = 25, size = 3, fill = "black") +
-  labs(y = "Canopy transmittance\n(Understorey light)", x = NULL) +
+  geom_point(data = light_dayly_co2, aes(x = Date, y = PAR), col = "grey") +
+  geom_smooth(data = light_dayly_co2, aes(x = Date, y = PAR, col = co2),
+              method = "loess", span = .02, n = 2648, se = FALSE, size = .6)+
+  geom_point(data = suevey_date, aes(x = Date, y = 270), shape = 25, size = 3, fill = "black") +
+  labs(y = expression(PAR~(mu*mol~s^'-1'~m^'-2')), x = NULL) +
   science_theme +
+  theme(legend.position = "none")+
   geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
   scale_color_manual(values = c("blue", "red"), labels = c("Ambient", expression(eCO[2])))
 
   
 
 
-# Moisture & Temperature --------------------------------------------------
+# temperature -----------------------------------------------------------
+
+load("output/Data/FACE_air_variables.RData")  # airvar_day; run the above to obtain up-to-date data from HIEv
+
+head(airvar_day)
+temp_daily_co2 <- airvar_day %>%
+  mutate(co2 = factor(ifelse(ring %in% c("1", "4", "5"), "eCO2", "Ambient"))) %>% 
+  group_by(Date, co2) %>% 
+  summarise(temp = mean(airtemp2m_Mean, na.rm = TRUE))
+  
+
+
+fig_temp <- ggplot()+
+  geom_point(data = temp_daily_co2, aes(x = Date, y = temp), col = "grey") +
+  geom_smooth(data = temp_daily_co2, aes(x = Date, y = temp, col = co2, linetype = co2),
+              method = "loess", span = .02, n = 2648, se = FALSE, size = .6)+
+  scale_color_manual(values = c("blue", "red"), labels = c("Ambient", expression(eCO[2]))) +
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
+  labs(x = NULL, y = expression(Temperature~(degree*C))) +
+  science_theme +
+  theme(legend.position = "none")
+fig_temp
+
+
+
+
+# moisture --------------------------------------------------------------
 
 load("Data//SoilVariables/soil.var_ring.means.RData")
 
@@ -56,33 +77,9 @@ MT_dayly_co2 <- ring.means %>%
 
 
 
-# . temperature -----------------------------------------------------------
-
-
-fig_temp <- ggplot()+
-  geom_rect(data = MT_range, 
-            aes(xmin = start_date, xmax = end_date, ymin = -Inf, ymax = Inf),
-            fill = "gray70", col = NA) +
-  geom_line(dat = MT_dayly_co2, aes(x = Date, y = temp, col = co2, linetype = co2)) +
-  scale_color_manual(values = c("blue", "red"), labels = c("Ambient", expression(eCO[2]))) +
-  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
-  labs(x = NULL, y = expression(Soil~temperature~(degree*C))) +
-  science_theme +
-  theme(legend.position = "none")
-fig_temp
-
-
-
-
-# . moisture --------------------------------------------------------------
-
-
 fig_moist <- ggplot()+
-  geom_rect(data = MT_range, 
-            aes(xmin = start_date, xmax = end_date, ymin = -Inf, ymax = Inf),
-            fill = "gray70", col = NA)+
   geom_line(dat = MT_dayly_co2, aes(x = Date, y = moist, col = co2)) +
-  labs(x = NULL, y = "Soil moisture (%)") +
+  labs(x = NULL, y = "Moisture (%)") +
   geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), linetype = "dashed") +
   scale_color_manual(values = c("blue", "red"), labels = c("Ambient", expression(eCO[2]))) +
   science_theme +
@@ -120,8 +117,8 @@ for (i in 1:3){
   env_figs[[i]] <- env_figs[[i]] + theme(axis.text.x = element_blank())
 }
 
-env_figs[[1]] <- env_figs[[1]] +
-  theme(legend.position = c(.11, .18),
+env_figs[[3]] <- env_figs[[3]] +
+  theme(legend.position = c(.11, 1.1),
         legend.background = element_rect(fill = "white"))
 
 env_figs[[4]] <- env_figs[[4]] +
