0c084f5b93753231eeb020a4a2cb247780c98c65
ShunHasegawa <hasegawashun.virgo@googlemail.com>
2017-05-06 21:33:40 +1000

    C4:C3 analysis was added to the summary document

--- R/Analysis.R
+++ R/Analysis.R
@@ -176,6 +176,7 @@ source("R/fig_diversity_indices.R")  # diversity indices
 source("R/fig_dominent_spp.R")       # dominent spp
 source("R/Stats_C3_4_Grass.R")       # C3vsC4, LegvsNonLeg, NativevsIntro
 source("R/PFG_Proportaion.R")        # Grass vs Forb
+source("R/analysis_c43ratio.R")      # C4:C3 ratios
 source("R/fig_pfg_propotion.R")      # combine grassn and PFG proportino results to create a figure
 source("R/analysis_summary.R")       # summarise above analyses
 source("R/analysis_PRC_MDS.R")       # prncipal response curve analysis and MDS analysis
